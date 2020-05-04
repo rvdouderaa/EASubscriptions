f13825b21bdb6339fdc3f88ce65da8436306e61b
Roel Ouderaa, van der <code@buildthecloud.io>
2020-05-04 13:52:32 +0200

    this works

--- Subscription-actions.ps1
+++ Subscription-actions.ps1
@@ -17,62 +17,12 @@ $enrollmentaccounts = Invoke-RestMethod -uri $url -ContentType 'application/json
 
 $request = @"
 {
-  "displayName": "Dev Team Subscription",
+  "displayName": "Dev Team Subscription 101",
   "offerType": "MS-AZR-0017P",
-  "owners": [
-    {
-      "objectId": "$OBJECTID"
-    }
-  ]
 }
 "@
 
 
 $url =  "https://management.azure.com/providers/Microsoft.Billing/enrollmentAccounts/$ENROLLMENTACCOUNT/providers/Microsoft.Subscription/createSubscription?api-version=2018-03-01-preview"
-$enrollmentaccounts = Invoke-RestMethod -uri $url -ContentType 'application/json' -Headers $headers -Method 'POST' -Body $request
+$result = Invoke-RestMethod -uri $url -ContentType 'application/json' -Headers $headers -Method 'POST' -Body $request
 
-
-<#
-GET https://management.azure.com/providers/Microsoft.Billing/enrollmentAccounts?api-version=2018-03-01-preview
-
-
-GET https://management.azure.com/providers/Microsoft.Billing/enrollmentAccounts?api-version=2018-03-01-preview
-
-{
-    "value": [
-      {
-        "id": "/providers/Microsoft.Billing/enrollmentAccounts/747ddfe5-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-        "name": "747ddfe5-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-        "type": "Microsoft.Billing/enrollmentAccounts",
-        "properties": {
-          "principalName": "SignUpEngineering@contoso.com"
-        }
-      },
-      {
-        "id": "/providers/Microsoft.Billing/enrollmentAccounts/4cd2fcf6-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-        "name": "4cd2fcf6-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-        "type": "Microsoft.Billing/enrollmentAccounts",
-        "properties": {
-          "principalName": "BillingPlatformTeam@contoso.com"
-        }
-      }
-    ]
-  }
-
-
-
-  POST https://management.azure.com/providers/Microsoft.Billing/enrollmentAccounts/<enrollmentAccountObjectId>/providers/Microsoft.Subscription/createSubscription?api-version=2018-03-01-preview
-
-{
-  "displayName": "Dev Team Subscription",
-  "offerType": "MS-AZR-0017P",
-  "owners": [
-    {
-      "objectId": "<userObjectId>"
-    },
-    {
-      "objectId": "<servicePrincipalObjectId>"
-    }
-  ]
-}
-#>
